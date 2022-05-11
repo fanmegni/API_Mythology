f0cfbf075ca489949efc6169d6be93e28995cb76
Schaeffer <divino.schaeffer02@gmail.com>
2022-05-11 15:52:39 +0200

    simplification

---
+++ assets/php/formMod.php
@@ -0,0 +1,24 @@
+<?php
+
+$id = htmlspecialchars($_POST["id"]);
+$text = htmlspecialchars($_POST["text"]);
+
+$pdo = new PDO('sqlite:' . dirname(__FILE__) . '/database.db');
+try {
+    if ($id) {
+        $statement = $pdo->prepare('
+        UPDATE questions
+        SET question = (:text)
+        WHERE id=(:id)
+    ');
+        $statement->bindValue('id', $id);
+        $statement->bindValue("text",$text);
+        $statement->execute();
+    }
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
+
+header("Location: http://localhost:63343/API_Mythology/liste.php");
+exit();
